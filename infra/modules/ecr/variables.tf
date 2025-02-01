7ddebab89a05e882c2b4fe14aef8ce70c3c8789c
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-01 19:50:24 +0530

    Update variables.tf

--- infra/modules/ecr/variables.tf
+++ infra/modules/ecr/variables.tf
@@ -1,1 +1,11 @@
+variable "appointment_service_name" {
+  description = "The name of the appointment service ECR repository"
+  type        = string
+  default     = "appointment-service"  # Default value (optional)
+}
 
+variable "patient_service_name" {
+  description = "The name of the patient service ECR repository"
+  type        = string
+  default     = "patient-service"  # Default value (optional)
+}
