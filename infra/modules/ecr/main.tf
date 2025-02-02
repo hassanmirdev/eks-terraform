593115ef1928f701dabafa610f388bac132cf6ea
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-02 13:39:27 +0530

    Update main.tf

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,7 +1,3 @@
-resource "aws_ecr_repository" "app_service" {
-  name = var.appointment_service_name
-}
-
-resource "aws_ecr_repository" "patient_service" {
-  name = var.patient_service_name
+resource "aws_ecr_repository" "my-app-repo" {
+  name = var.repo_name
 }
