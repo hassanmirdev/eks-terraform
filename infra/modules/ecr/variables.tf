6322d4366950922fedf2db5591d5c16df598a09d
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-02 13:42:01 +0530

    Update variables.tf

--- infra/modules/ecr/variables.tf
+++ infra/modules/ecr/variables.tf
@@ -1,11 +1,5 @@
-variable "appointment_service_name" {
-  description = "The name of the appointment service ECR repository"
+variable "repo_name" {
+  description = "Name of the ECR repository"
   type        = string
-  default     = "appointment-service"  # Default value (optional)
-}
-
-variable "patient_service_name" {
-  description = "The name of the patient service ECR repository"
-  type        = string
-  default     = "patient-service"  # Default value (optional)
+  default     = "my-app-repo"
 }
